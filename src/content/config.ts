9aba887b0bddd6726e80ab36cf77d148c3daed67
Tanarak Chunsanit <tanarak@Tanaraks-MacBook-Pro.local>
2024-07-26 21:31:16 +0700

    add articles

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,14 +4,15 @@ import { z, defineCollection } from "astro:content";
 // 2. Define a `type` and `schema` for each collection
 const blogCollection = defineCollection({
   type: "content", // v2.5.0 and later
-  schema: z.object({
-    title: z.string(),
-    date: z.string(),
-    image: z.string(),
-    desc: z.string(),
-    author: z.string(),
-    tags: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      desc: z.string(),
+      image: image(),
+      author: z.string(),
+      tags: z.array(z.string()),
+    }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
